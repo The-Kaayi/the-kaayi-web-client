46f6947c0794a58ec3070c365a7b6e96fbe32748
Syed Ali <sydali3103@gmail.com>
2024-04-26 03:04:50 +0530

    basic hero section structure

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,13 +1,24 @@
-import styles from './HeroSection.module.scss'
+import Image from "next/image";
+import heroImage from "../../../public/images/main-logo.png";
+import styles from "./HeroSection.module.scss";
 
 const HeroSection: React.FC = () => {
+  return (
+    <section className={styles.heroSection}>
+      <div className={styles.imageContainer}>
+        <Image src={heroImage} alt="Hero Image" width={500} height={500} />
+      </div>
+      <p className={styles.tagline}>
+        Explore, rate, and relish local flavors.
+      </p>
+      <h2 className={styles.title}>Feast on Feedback</h2>
+      <p className={styles.description}>
+        Share your dining experiences and explore local favorites. Connect with
+        the community, discover new flavors, and support your favorite spots.
+      </p>
+      <button className={styles.ctaBtn}>Get Started</button>
+    </section>
+  );
+};
 
-    return ( 
-        <section className={styles.heroSection}>
-            <h1 className={styles.title}>The Kaayi</h1>
-        </section>
-    )
-        
-}
-
-export default HeroSection
+export default HeroSection;
