358731d7a35c64efa35ce14f2b05ac3cdccf5114
Syed Ali <sydali3103@gmail.com>
2024-04-28 18:23:44 +0530

    hero background change

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -5,18 +5,23 @@ import styles from "./HeroSection.module.scss";
 const HeroSection: React.FC = () => {
   return (
     <section className={styles.heroSection}>
-      <div className={styles.imageContainer}>
-        <Image src={heroImage} alt="Hero Image" width={500} height={500} />
+      <div className={styles.filter}>
+        <div className={styles.imageContainer}>
+          <Image src={heroImage} alt="Hero Image" width={500} height={500} />
+        </div>
+        <div className={styles.tagline}>
+          <p>Elevate</p>
+          <p>Connect</p>
+          <p>Thrive</p>
+        </div>
+        <h2 className={styles.title}>Feast on Feedback</h2>
+        <p className={styles.description}>
+          Share your dining experiences and explore local favorites. Connect
+          with the community, discover new flavors, and support your favorite
+          spots.
+        </p>
+        <button className={styles.ctaBtn}>Get Started</button>
       </div>
-      <p className={styles.tagline}>
-        Explore, rate, and relish local flavors.
-      </p>
-      <h2 className={styles.title}>Feast on Feedback</h2>
-      <p className={styles.description}>
-        Share your dining experiences and explore local favorites. Connect with
-        the community, discover new flavors, and support your favorite spots.
-      </p>
-      <button className={styles.ctaBtn}>Get Started</button>
     </section>
   );
 };
