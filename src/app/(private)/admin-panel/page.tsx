548706fddfa2501590e733ff94d1f27fdcd1fad3
Syed Ali <sydali3103@gmail.com>
2024-05-19 18:11:12 +0530

    admin panel route created

---
+++ src/app/(private)/admin-panel/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss";
+
+const AdminPanel: React.FC = () => {
+  return (
+    <div className={styles.adminPanel}>
+      <h1>Admin Panel</h1>
+    </div>
+  );
+};
+
+export default AdminPanel;
