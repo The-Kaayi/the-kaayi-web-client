63df5ab8526b51149400f8c52f1bfb428b537d1b
Syed Ali <sydali3103@gmail.com>
2024-05-21 04:39:10 +0530

    sidebar content added

--- src/components/AdminSideBar/AdminSideBar.tsx
+++ src/components/AdminSideBar/AdminSideBar.tsx
@@ -1,14 +1,124 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
+import classNames from "classnames";
+import { Menu } from "antd";
+import type { MenuProps } from "antd";
 import appLogo from "../../../public/app-logo.svg";
+import overviewIcon from "../../../public/images/AdminSideBar/overview.svg";
+import missionsIcon from "../../../public/images/AdminSideBar/missions.svg";
+import rewardsIcon from "../../../public/images/AdminSideBar/rewards.svg";
+import settingsIcon from "../../../public/images/AdminSideBar/settings.svg";
 import styles from "./AdminSideBar.module.scss";
 
-const AdminSideBar: React.FC = () => {
+const Sidebar: React.FC<{}> = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [currentPath, setCurrentPath] = useState("dashboard");
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const segments = pathname.split("/");
+    const path =
+      segments[1] === "en" && segments.length > 2
+        ? segments[2]
+        : segments[1] || "dashboard";
+    setCurrentPath(path);
+  }, [pathname]);
+
+  const localizedSidebarItems: MenuProps["items"] = [
+    {
+      label: "Overview",
+      key: "admin-panel",
+      icon: (
+        <Image
+          src={overviewIcon}
+          alt="Overview Icon"
+          width={25}
+        />
+      ),
+    },
+    {
+      label: "Missions",
+      key: "missions",
+      icon: (
+        <Image
+          src={missionsIcon}
+          alt="Missions Icon"
+          width={25}
+        />
+      ),
+    },
+    {
+      label: "Rewards",
+      key: "rewards",
+      icon: (
+        <Image
+          src={rewardsIcon}
+          alt="Rewards Icon"
+          width={25}
+        />
+      ),
+    },
+  ];
+
+  const localizedSidebarBottomItems = [
+    {
+      label: "Settings",
+      key: "settings",
+      icon: <Image src={settingsIcon} alt="Settings Icon" width={25} />,
+    },
+  ];
+
+  const handleMouseEnter = () => {
+    setIsActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsActive(false);
+  };
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCurrentPath(e.key);
+    router.push(`/${e.key}`);
+  };
+
   return (
-    <div className={styles.adminSideBar}>
-      <Image className={styles.appLogo} src={appLogo} alt="App Logo" />
-      <h2>Admin Side Bar</h2>
-    </div>
+    <aside
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className={classNames(styles.sidebar, {
+        [styles.sidebarActive]: isActive,
+      })}
+    >
+      <div className={styles.header}>
+        <div className={styles.logoContainer}>
+          <Image className={styles.appLogo} src={appLogo} alt="App Logo" />
+        </div>
+      </div>
+      <div
+        className={classNames(styles.sidebarContainer, {
+          [styles.sidebarActive]: isActive,
+        })}
+      >
+        <Menu
+          mode="inline"
+          className={styles.sideOptions}
+          onClick={onClick}
+          selectedKeys={[currentPath]}
+          items={localizedSidebarItems}
+        ></Menu>
+        <Menu
+          mode="inline"
+          onClick={onClick}
+          selectedKeys={[currentPath]}
+          items={localizedSidebarBottomItems}
+        ></Menu>
+      </div>
+    </aside>
   );
 };
 
-export default AdminSideBar;
+export default Sidebar;
