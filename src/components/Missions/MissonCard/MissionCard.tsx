f20aae7fdaa139ed9e82268cd41cf99ce54c5049
Syed Ali <sydali3103@gmail.com>
2024-05-21 22:25:23 +0530

    dynamic routing for mission cards

--- src/components/Missions/MissonCard/MissionCard.tsx
+++ src/components/Missions/MissonCard/MissionCard.tsx
@@ -2,16 +2,17 @@ import Link from "next/link";
 import styles from "./MissionCard.module.scss";
 
 const MissionCard: React.FC<{ missionUUID: string }> = ({ missionUUID }) => {
-
-  const onCardClick = () => {
-    
-  }
   return (
     <div className={styles.missionCard}>
       <p>Mission Card {missionUUID}</p>
-      <Link href={`missions/${missionUUID}`} className={styles.viewBtn}>
-        View Details
-      </Link>
+      <div className={styles.btnContainer}>
+        <Link href={`missions/${missionUUID}`} className={styles.viewBtn}>
+          View 
+        </Link>
+        <Link href={`missions/${missionUUID}/edit`} className={styles.editBtn}>
+          Edit
+        </Link>
+      </div>
     </div>
   );
 };
