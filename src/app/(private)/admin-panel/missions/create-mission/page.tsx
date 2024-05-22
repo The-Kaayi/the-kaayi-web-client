4c7b8249d3f4b6a5f5f78d1c7371aa9e79912b3c
Syed Ali <sydali3103@gmail.com>
2024-05-22 13:42:51 +0530

    back link added in create page

--- src/app/(private)/admin-panel/missions/create-mission/page.tsx
+++ src/app/(private)/admin-panel/missions/create-mission/page.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
+import arrowIcon from "../../../../../../public/images/Missions/arrow-right.svg";
 import styles from "./page.module.scss";
 
 const CreateMission: React.FC = () => {
   return (
     <div className={styles.createMission}>
-      <h1>Create Mission</h1>
-      <Link href="/admin-panel/missions">Back</Link>
+      <div className={styles.titleContainer}>
+        <h2 className={styles.title}>Create a new mission</h2>
+        <Link className={styles.backBtn} href="/admin-panel/missions">
+          <Image className={styles.backIcon} src={arrowIcon} alt="Arrow Icon" />
+          Go Back
+        </Link>
+      </div>
     </div>
   );
 };
