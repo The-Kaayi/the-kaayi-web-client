c56b1190986df7e0d94a42cbb9379448a423f95d
Syed Ali <sydali3103@gmail.com>
2024-05-22 14:17:53 +0530

    mission create form

--- src/app/(private)/admin-panel/missions/create-mission/page.tsx
+++ src/app/(private)/admin-panel/missions/create-mission/page.tsx
@@ -13,6 +13,45 @@ const CreateMission: React.FC = () => {
           Go Back
         </Link>
       </div>
+
+      <div className={styles.content}>
+        <div className={styles.shopDetails}>
+          <h3 className={styles.title}>Shop Details</h3>
+          <div className={styles.details}>
+            <div className={styles.subContainer}>
+              <label className={styles.label}>Name</label>
+              <input className={styles.input} type="text" placeholder="Name" />
+            </div>
+
+            <div className={styles.subContainer}>
+              <label className={styles.label}>Location</label>
+              <input
+                className={styles.input}
+                type="text"
+                placeholder="Location"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.missionDetails}>
+          <h3 className={styles.title}>Mission Details</h3>
+          <div className={styles.details}>
+            <div className={styles.subContainer}>
+              <label className={styles.label}>Title</label>
+              <input className={styles.input} type="text" placeholder="Title" />
+            </div>
+
+            <div className={styles.subContainer}>
+              <label className={styles.label}>Description</label>
+              <textarea
+                className={styles.textarea}
+                placeholder="Description"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
