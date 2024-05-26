2f5c72b993b0a68e5bbb6108a70e0c6c1349e25c
Syed Ali <sydali3103@gmail.com>
2024-05-27 05:01:57 +0530

    long and short paragraph styling

--- src/components/Question/ShortParagraph/ShortParagraph.tsx
+++ src/components/Question/ShortParagraph/ShortParagraph.tsx
@@ -3,7 +3,10 @@ import styles from "./ShortParagraph.module.scss";
 const ShortParagraph: React.FC = () => {
   return (
     <div className={styles.shortParagraph}>
-      <p>Short Question</p>
+      <input className={styles.question} type="text" placeholder="Question" />
+      <div className={styles.answerBox}>
+        <input className={styles.answer}  type="text" placeholder="Short Paragraph" readOnly />
+      </div>
     </div>
   );
 };
